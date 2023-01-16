51832f5cc85c88d45ed5170c837d1a6e7bc9497b
Faizan Arif <faizanarif1245@gmail.com>
2023-01-17 00:50:07 +0500

    Array Traversal added

---
+++ Array/array_traversal.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[5]={2,3,6,7,1};
+    cout<<"Array Traversal is: ";
+    for(int i=0;i<5;i++){
+        cout<<arr[i]<<"\t";
+    }
+    return 0;
+}
